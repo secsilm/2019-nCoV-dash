a6ddb56ee8451f32be5537bbd6f3e5cf8ac82f1d
secsilm <secsilm@outlook.com>
2020-02-12 13:08:06 +0800

    优化界面及配色；优化地名

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # 2019-nCoV 疫情展示
 
 本项目用于展示新型冠状病毒 2019-nCoV 疫情展示，使用 [dash](https://plot.ly/dash/) 制作。
+
+## References
+
+- [Mapbox Map Layers | Python | Plotly](https://plot.ly/python/mapbox-layers/)
+- [Mapbox Choropleth Maps | Python | Plotly](https://plot.ly/python/mapbox-county-choropleth/#choropleth-map-using-plotlygraphobjects-and-carto-base-map-no-token-needed)
--- app.py
+++ app.py
@@ -1,16 +1,5 @@
 """
-关于 [mapbox_style](https://plot.ly/python/mapbox-layers/)：
-
-The accepted values for layout.mapbox.style are one of:
-
-- "white-bg" yields an empty white canvas which results in no external HTTP requests
-"open-street-map", "carto-positron", "carto-darkmatter", "stamen-terrain", "stamen-toner" or "stamen-watercolor" yeild maps composed of raster tiles from various public tile servers which do not require signups or access tokens
-
-- "basic", "streets", "outdoors", "light", "dark", "satellite", or "satellite-streets" yeild maps composed of vector tiles from the Mapbox service, and do require a Mapbox Access Token or an on-premise Mapbox installation.
-
-- A Mapbox service style URL, which requires a Mapbox Access Token or an on-premise Mapbox installation.
-
-- A Mapbox Style object as defined at https://docs.mapbox.com/mapbox-gl-js/style-spec/
+Dash app.
 """
 
 import json
@@ -18,40 +7,43 @@ from datetime import datetime
 
 import dash
 import dash_core_components as dcc
-import dash_html_components as html
 import dash_daq as daq
-import dash_table
+import dash_html_components as html
 import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
-from plotly.subplots import make_subplots
 import requests
 from dash.dependencies import Input, Output
-from plotly import tools
+from plotly.subplots import make_subplots
+from fake_useragent import UserAgent
+
+import config
 
 app = dash.Dash(__name__)
-colors = ["#E51017", "#FA893A", "#307D47", "#0A0603"]
-with open(".mapboxtoken", "r") as f:
-    token = f.read()
+colors = ["#E51017", "#FA893A", "#307D47", "#FFFFFF"]
 
-with open("china_provinces.geojson") as f:
+ua = UserAgent()
+
+with open("data/china_provinces_v2.geojson") as f:
     provinces_map = json.load(f)
-with open("china_cities_github.geojson") as f:
+with open("data/china_cities_github.geojson") as f:
     cities_map = json.load(f)
+with open("data/description.md", "r", encoding="utf-8") as f:
+    description = f.read()
 
-apis = {
-    "qq": "https://service-n9zsbooc-1252957949.gz.apigw.tencentcs.com/release/qq",
-    "dxy": "https://service-0gg71fu4-1252957949.gz.apigw.tencentcs.com/release/dingxiangyuan",
-    "province_city_history": "http://ncov.nosensor.com:8080/api/",
-}
+# print(f"香港 in provinces_map={'香港' in provinces_map.NL_NAME_1.values}")
+# print(f"香港 in cities_map={'香港' in cities_map.NAME.values}")
 
 # 虽然直辖市为省级，但是此处仍将其纳入市级来展示
 # 为了方便起见，台湾也计入其中
-municipalities = ['北京', '上海', '天津', '重庆', '台湾']
+municipalities = ["北京", "上海", "天津", "重庆", "台湾", "香港"]
 
 
-def timestamp2datetime(t):
-    return pd.datetime.fromtimestamp(t).strftime("%Y-%m-%d %H:%M:%S")
+def uniform_city_name(cn):
+    if cn not in config.exceptions:
+        cn = cn.rstrip("地区")
+    cn = config.replace_map.get(cn, cn)
+    return cn
 
 
 app.title = f"新型冠状病毒 2019-nCoV 疫情趋势"
@@ -59,8 +51,11 @@ app.layout = html.Div(
     [
         html.H1(children=f"新型冠状病毒 2019-nCoV 疫情趋势", style={"marginLeft": "3%"}),
         html.Div(id="update-time-text", style={"marginLeft": "3%"}),
+        dcc.Markdown(
+            description, style={"marginLeft": "3%", "marginRight": "3%"}, highlight_config={"theme": "dark"}
+        ),
         html.Div(
-            id="number_plate",
+            id="counts",
             style={
                 "marginLeft": "5%",
                 "marginRight": "5%",
@@ -82,6 +77,8 @@ app.layout = html.Div(
                             color=colors[0],
                             size=50,
                             labelPosition="bottom",
+                            theme={'dark': True},
+                            backgroundColor='black'
                         )
                     ],
                 ),
@@ -99,6 +96,7 @@ app.layout = html.Div(
                             color=colors[1],
                             size=50,
                             labelPosition="bottom",
+                            backgroundColor='black'
                         )
                     ],
                 ),
@@ -116,6 +114,7 @@ app.layout = html.Div(
                             color=colors[2],
                             size=50,
                             labelPosition="bottom",
+                            backgroundColor='black'
                         )
                     ],
                 ),
@@ -133,12 +132,15 @@ app.layout = html.Div(
                             color=colors[3],
                             size=50,
                             labelPosition="bottom",
+                            backgroundColor='black'
                         )
                     ],
                 ),
             ],
         ),
-        dcc.Interval(id="interval-component", interval=10 * 60 * 1000, n_intervals=0),
+        dcc.Interval(
+            id="interval-component", interval=config.update_interval, n_intervals=0
+        ),
         dcc.Graph(
             id="trend",
             style={
@@ -170,14 +172,15 @@ app.layout = html.Div(
                 "marginTop": "2%",
                 "marginBottom": "2%",
             },
-        )
-    ]
+        ),
+    ],
 )
 
 
 @app.callback(Output("trend", "figure"), [Input("interval-component", "n_intervals")])
 def update_graph(n):
-    r = requests.get(apis["qq"])
+    headers = {"User-Agent": ua.random}
+    r = requests.get(config.apis["qq"], headers=headers)
     r.raise_for_status()
     res = r.json()
     data = res["data"]["wuwei_ww_cn_day_counts"]
@@ -228,7 +231,7 @@ def update_graph(n):
     fig.append_trace(trace_dead, 2, 1)
     fig.append_trace(trace_cured, 2, 1)
     margin = go.layout.Margin(l=100, r=100, b=50, t=25, pad=4)
-    fig["layout"].update(margin=margin, showlegend=True, template="plotly")
+    fig["layout"].update(margin=margin, showlegend=True, template="plotly_dark")
     return fig
 
 
@@ -243,16 +246,53 @@ def update_graph(n):
     [Input("interval-component", "n_intervals")],
 )
 def update_counts(n):
-    r = requests.get(apis["qq"])
+    headers = {"User-Agent": ua.random}
+    r = requests.get(config.apis["qq"], headers=headers)
     r.raise_for_status()
     res = r.json()
+    with open(
+        f"data/qq_{datetime.now().strftime('%Y%m%d')}.json",
+        "w",
+        encoding="utf-8",
+    ) as f:
+        json.dump(res, f, ensure_ascii=False, indent=4)
     confirmed = res["data"]["wuwei_ww_global_vars"][0]["confirmCount"]
     suspected = res["data"]["wuwei_ww_global_vars"][0]["suspectCount"]
     dead = res["data"]["wuwei_ww_global_vars"][0]["deadCount"]
     cured = res["data"]["wuwei_ww_global_vars"][0]["cure"]
     update_time = res["data"]["wuwei_ww_global_vars"][0]["update_time"]
+
+    # 省市每日历史数据
+    try:
+        r = requests.get("http://ncov.nosensor.com:8080/api/", headers=headers)
+        r.raise_for_status()
+        res = r.json()
+        with open(
+            f"data/province_city_history_{datetime.now().strftime('%Y%m%d')}.json",
+            "w",
+            encoding="utf-8",
+        ) as f:
+            json.dump(res, f, ensure_ascii=False, indent=4)
+    except Exception as e:
+        print(f"保存省市每日历史数据出错。{e}")
+    # 丁香园每分钟历史数据
+    try:
+        r = requests.get(
+            "http://lab.isaaclin.cn/nCoV/api/area?latest=0", headers=headers
+        )
+        r.raise_for_status()
+        res = r.json()
+        with open(
+            f"data/dxy_minutes_history_{datetime.now().strftime('%Y%m%d')}.json",
+            "w",
+            encoding="utf-8",
+        ) as f:
+            json.dump(res, f, ensure_ascii=False, indent=4)
+    except Exception as e:
+        print(f"保存丁香园每分钟历史数据出错。{e}")
+
     return (
-        f'更新时间：{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}',
+        f'更新时间：{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}，每 {config.update_interval // 60000} 分钟更新一次。',
         f"{confirmed:05d}",
         f"{suspected:05d}",
         f"{dead:05d}",
@@ -260,9 +300,12 @@ def update_counts(n):
     )
 
 
-@app.callback(Output("province-level-map", "figure"), [Input("interval-component", "n_intervals")])
+@app.callback(
+    Output("province-level-map", "figure"), [Input("interval-component", "n_intervals")]
+)
 def update_province_map(n):
-    r = requests.get(apis["dxy"])
+    headers = {"User-Agent": ua.random}
+    r = requests.get(config.apis["dxy"], headers=headers)
     r.raise_for_status()
     res = r.json()
     data = res["data"]["getAreaStat"]
@@ -278,6 +321,17 @@ def update_province_map(n):
             for i in data
         ]
     )
+    df = pd.DataFrame(
+        data={
+            "provinces": provinces,
+            "confirmeds": confirmeds,
+            "suspecteds": suspecteds,
+            "cureds": cureds,
+            "deads": deads,
+        },
+        columns=["provinces", "confirmeds", "suspecteds", "cureds", "deads"],
+    )
+    df.to_csv("provinces_data.csv", index=False, encoding="utf8")
     confirmeds_log = np.log(np.add(confirmeds, 1))
     fig = go.Figure(
         go.Choroplethmapbox(
@@ -288,7 +342,7 @@ def update_province_map(n):
             # zmin=0,
             # zmax=1000,
             zauto=True,
-            colorscale="Reds",
+            colorscale="BuPu",
             reversescale=True,
             marker_opacity=0.8,
             marker_line_width=0.8,
@@ -299,41 +353,52 @@ def update_province_map(n):
             + "治愈：%{customdata[3]}<br>"
             + "死亡：%{customdata[4]}<br>"
             + "<extra></extra>",
+            showscale=False
         )
     )
     fig.update_layout(
         mapbox_style="carto-darkmatter",
         mapbox_zoom=3,
         mapbox_center={"lat": 35.110573, "lon": 106.493924},
-        mapbox_accesstoken=token,
+        mapbox_accesstoken=config.token,
     )
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     return fig
 
 
-@app.callback(Output("city-level-map", "figure"), [Input("interval-component", "n_intervals")])
+@app.callback(
+    Output("city-level-map", "figure"), [Input("interval-component", "n_intervals")]
+)
 def update_city_map(n):
-    r = requests.get(apis["dxy"])
+    headers = {"User-Agent": ua.random}
+    r = requests.get(config.apis["dxy"], headers=headers)
     r.raise_for_status()
     res = r.json()
+    with open(
+        f"data/dxy_{datetime.now().strftime('%Y%m%d')}.json",
+        "w",
+        encoding="utf-8",
+    ) as f:
+        json.dump(res, f, ensure_ascii=False, indent=4)
     data = res["data"]["getAreaStat"]
     cities, confirmeds, suspecteds, cureds, deads = [], [], [], [], []
     for province in data:
         # print(f"正在处理 {province['provinceShortName']} ...")
-        if province['provinceShortName'] in municipalities:
+        if province["provinceShortName"] in municipalities:
             # print(f"直辖市：{province['provinceShortName']}")
-            cities.append(province['provinceShortName'])
-            confirmeds.append(province['confirmedCount'])
+            cities.append(province["provinceShortName"])
+            confirmeds.append(province["confirmedCount"])
             suspecteds.append(province["suspectedCount"])
             cureds.append(province["curedCount"])
             deads.append(province["deadCount"])
             continue
-        for city in province['cities']:
+        for city in province["cities"]:
             cities.append(city["cityName"])
             confirmeds.append(city["confirmedCount"])
             suspecteds.append(city["suspectedCount"])
             cureds.append(city["curedCount"])
             deads.append(city["deadCount"])
+    cities = [uniform_city_name(cn) for cn in cities]
     confirmeds_log = np.log(np.add(confirmeds, 1))
     df = pd.DataFrame(
         data={
@@ -341,20 +406,18 @@ def update_city_map(n):
             "confirmeds": confirmeds,
             "suspecteds": suspecteds,
             "cureds": cureds,
-            "deads": deads
+            "deads": deads,
         }
     )
-    df.to_csv('cities_data.csv', index=False, encoding='utf8')
+    df.to_csv("cities_data.csv", index=False, encoding="utf8")
     fig = go.Figure(
         go.Choroplethmapbox(
             featureidkey="properties.NAME",
             geojson=cities_map,
             locations=cities,
             z=confirmeds_log,
-            # zmin=0,
-            # zmax=1000,
             zauto=True,
-            colorscale="Reds",
+            colorscale="PuRd",
             reversescale=True,
             marker_opacity=0.8,
             marker_line_width=0.8,
@@ -365,16 +428,18 @@ def update_city_map(n):
             + "治愈：%{customdata[3]}<br>"
             + "死亡：%{customdata[4]}<br>"
             + "<extra></extra>",
+            showscale=False
         )
     )
     fig.update_layout(
         mapbox_style="carto-darkmatter",
         mapbox_zoom=3,
         mapbox_center={"lat": 35.110573, "lon": 106.493924},
-        mapbox_accesstoken=token,
+        mapbox_accesstoken=config.token,
     )
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     return fig
+
 
 if __name__ == "__main__":
     app.run_server(host="0.0.0.0", port=9102, debug=False)
